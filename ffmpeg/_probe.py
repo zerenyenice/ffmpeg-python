e993a4aff3f137ac64c0facae764c920f850772f
zerenyenice <zerendyenice@gmail.com>
2021-02-17 11:54:03 +0300

    Update _probe.py
    added ffprobe option for getting key frames inside a list

--- ffmpeg/_probe.py
+++ ffmpeg/_probe.py
@@ -26,5 +26,25 @@ def probe(filename, cmd='ffprobe', timeout=None, **kwargs):
         raise Error('ffprobe', out, err)
     return json.loads(out.decode('utf-8'))
 
+def probe_key_frames(filename, cmd='ffprobe', **kwargs):
+    """Run this ffprobe on the specified file with key frame options and return a csv representation of the key frames.
+
+    Raises:
+        :class:`ffmpeg.Error`: if ffprobe returns a non-zero exit code,
+            an :class:`Error` is returned with a generic error message.
+            The stderr output can be retrieved by accessing the
+            ``stderr`` property of the exception.
+    """
+    args = [cmd, '-show_format', '-show_streams', '-of', 'csv']
+    args += convert_kwargs_to_cmd_line_args(kwargs)
+    args += [filename]
+
+    p1 = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    p2 = subprocess.Popen(['grep','-n','I'], stdin=p1.stdout ,stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = p2.communicate()
+    if p2.returncode != 0:
+        raise Error('ffprobe', out, err)
+    return [int(x.split(':')[0]) for x in out.decode('utf-8').splitlines()]
+
 
 __all__ = ['probe']
